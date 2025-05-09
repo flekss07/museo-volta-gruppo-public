c19e57a6992b222af2bd5b0a85f5c64d210f3b8c
flekss07 <edopia007@gmail.com>
2025-05-09 09:40:50 +0200

    aggiunta view per form e file forms per effettuare login, da ultimare

--- django/museo/views.py
+++ django/museo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import LoginForm
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,9 @@ def index(request):
 
 def galleria(request):
     return render(request,"galleria.html")
+
+# richiesta per pagina di login
+def login(request):
+    context = {} # array di contenuti da passare a pagina
+    context["form"] = LoginForm() # aggiunge form ad array
+    return render(request,"login.html",context) #passa dati a pagina
