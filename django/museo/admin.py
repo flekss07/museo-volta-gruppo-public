449f8c42d1acdf0b2b95a5e0a229538b59b8507f
flekss07 <edopia007@gmail.com>
2025-05-12 13:11:05 +0200

    fatte migrazioni a admin

--- django/museo/admin.py
+++ django/museo/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Opera, User
+from .models import Opera, User, Slot, Giocata, JackPot
 
 # Register your models here.
 
 admin.site.register(Opera)
 admin.site.register(User)
+admin.site.register(Slot)
+admin.site.register(Giocata)
+admin.site.register(JackPot)
