d99c399033bc4706e6ddbfc23094c735d85413cc
flekss07 <edopia007@gmail.com>
2025-05-12 12:11:43 +0200

    fatto wiring con django e homepage

--- django/museo/urls.py
+++ django/museo/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("galleria/", views.galleria, name="galleria"),
     path("login/", views.login,name="login"),
-    path("registrazione/",views.login,name="registrazione")
+    path("registrazione/",views.login,name="registrazione"),
+    path("slot/",views.slot,name="slot")
 ]
